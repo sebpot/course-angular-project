8d89b03656c1036e8d99bebbfcbe0fd9a9762607
Sebastian Potrykus <seba05092001@gmail.com>
2023-07-29 17:39:47 +0200

    Added recipe, shopping list services and usage of dependency injection

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,19 +1,19 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent {
-  @Output() recipeWasSelected = new EventEmitter<Recipe>();
-  recipes: Recipe[] = [
-    new Recipe('Rat', 'Good stuff', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=556,505')
-  ];
+export class RecipeListComponent implements OnInit{
+  recipes: Recipe[];
 
-  onRecipeSelected(selectedRecipe: Recipe){
-    this.recipeWasSelected.emit(selectedRecipe);
+  constructor(private recipeService: RecipeService){}
+
+  ngOnInit() {
+    this.recipes = this.recipeService.getRecipes();
   }
 }
---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,35 @@
+import { EventEmitter, Injectable } from "@angular/core";
+
+import { Recipe } from "./recipe.model";
+import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
+
+@Injectable()
+export class RecipeService {
+    recipeSelected = new EventEmitter<Recipe>();
+
+    private recipes: Recipe[] = [
+        new Recipe('Ratatouille', 'Good stuff', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=556,505', 
+        [  
+            new Ingredient('Carrot', 3),
+            new Ingredient('Pepper', 6),
+            new Ingredient('Cucumber', 3)
+        ]),
+        new Recipe('Pizza', 'Better stuff', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=556,505', 
+        [  
+            new Ingredient('Cheese', 6),
+            new Ingredient('Pepper', 3),
+            new Ingredient('Onion', 2)
+        ])
+    ];
+
+    constructor(private slService: ShoppingListService){}
+
+    getRecipes() {
+        return this.recipes.slice();
+    }
+
+    addIngredientsToShoppingList(ingredients: Ingredient[]){
+        this.slService.addIngredients(ingredients);
+    }
+}
